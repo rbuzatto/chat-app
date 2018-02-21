9a37035b6f7ea1534d512b94443a0a9189cb78fa
rafael bc <mightyrafa@gmail.com>
2018-02-20 22:52:27 -0300

    Add connect/disconnect evts

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
-const path = require('path');
-const express = require('express');
+const path      = require('path');
+const http      = require('http');
+const express   = require('express');
+const socketIO  = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
 const port = process.env.PORT || 3000;
 let app = express();
+// express().listen() chama o  .createServer() de http
+let server = http.createServer(app); //tudo isso para poder incorp. socket io...
+let io = socketIO(server); //return websocket server
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user is now connected');
+
+    socket.on('disconnect', () => {
+        console.log('User got disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
